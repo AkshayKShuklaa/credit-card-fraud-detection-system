231d40db8cfb62a39bbda59684fde381a13ef172
AkshayKShuklaa <2k22.csaiml.32598@gmail.com>
2026-05-10 23:48:25 +0530

    Fix production backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://credit-card-fraud-detection-system-1-2f29.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
